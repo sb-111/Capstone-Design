edb99e5e9f03b7e345c6aec64ba7ce9441c3fd92
aodlf452 <aodlf452@gmail.com>
2024-03-03 14:46:50 +0900

    portal spawn

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -9,8 +9,10 @@ public class Countdown : MonoBehaviour
     [SerializeField] float setTime = 100.0f;
     [SerializeField] Text countdownText;
     [SerializeField] GameObject Player;
-
+    [SerializeField] SpawnPortal spawnPortal;
     int playerCount = 0;
+
+    private bool portalSpawned = false;
 
     void Start()
     {
@@ -23,7 +25,13 @@ public class Countdown : MonoBehaviour
     {
         playerCount = PhotonNetwork.PlayerList.Length;
 
-        if (Player != null && playerCount == 2)
+        if (Player != null && playerCount == 2 && !portalSpawned)
+        {
+            StartCoroutine(DelayedSpawn(spawnPortal.spawnDelay)); // 코루틴을 사용하여 일정 시간 뒤에 포탈을 생성하도록 합니다.
+            portalSpawned = true; // 포탈이 생성되었음을 표시합니다.
+        }
+
+        if (playerCount == 2)
         {
             if (setTime > 0)
             {
@@ -38,4 +46,12 @@ public class Countdown : MonoBehaviour
             countdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
         }
     }
+
+    // 일정 시간 뒤에 포탈을 생성하는 코루틴입니다.
+    IEnumerator DelayedSpawn(float delay)
+    {
+        yield return new WaitForSeconds(delay); // 지정된 시간만큼 대기합니다.
+        spawnPortal.SpawnObject(); // 대기 시간이 끝나면 포탈을 생성합니다.
+    }
+
 }
--- Assets/Scripts/SpawnPortal.cs
+++ Assets/Scripts/SpawnPortal.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class SpawnPortal : MonoBehaviour
 {
     public GameObject objectToSpawn;
-    public float spawnDelay = 5.0f;
+    public float spawnDelay =10.0f;
 
     void Start()
     {
-        Invoke("SpawnObject", spawnDelay);
+        //Invoke("SpawnObject", spawnDelay);
     }
 
     // 오브젝트를 생성하는 메서드입니다.
-    void SpawnObject()
+    public void SpawnObject()
     {
         Instantiate(objectToSpawn, transform.position, Quaternion.identity);
     }
