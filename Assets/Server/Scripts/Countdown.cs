edae9c9df6f1ed31b9ada3dce4b2e6400453f0de
aodlf452 <aodlf452@gmail.com>
2024-03-22 22:56:33 +0900

    Script 분리
    Countdown.cs // SpawnPortal.cs 코드 분리
    time 동기화 실패

--- Assets/Server/Scripts/Countdown.cs
+++ Assets/Server/Scripts/Countdown.cs
@@ -9,7 +9,6 @@ public class Countdown : MonoBehaviour
     [SerializeField] int setTime = 100;
     [SerializeField] Text countdownText;
     [SerializeField] GameObject Player;
-    [SerializeField] SpawnPortal spawnPortal;
     int playerCount = 0;
     int mode = 0;
     private int time;
@@ -20,47 +19,23 @@ public class Countdown : MonoBehaviour
     void Start()
     {
         PV = GetComponent<PhotonView>();
-        int initialMinutes = Mathf.FloorToInt(setTime / 60); // 시작할 때의 분
-        int initialSeconds = Mathf.FloorToInt(setTime - initialMinutes * 60); // 시작할 때의 초
-        countdownText.text = string.Format("{0:00}:{1:00}", initialMinutes, initialSeconds); // 시작할 때의 시간을 텍스트로 설정
+        /*int initialMinutes = Mathf.FloorToInt(setTime / 60);
+        int initialSeconds = Mathf.FloorToInt(setTime - initialMinutes * 60);
+        countdownText.text = string.Format("{0:00}:{1:00}", initialMinutes, initialSeconds);
+    */
     }
 
     void Update()
     {
         playerCount = PhotonNetwork.PlayerList.Length;
-        if (Player != null && playerCount == 1 && !portalSpawned)
-        {
-            StartCoroutine(DelayedSpawn(spawnPortal.spawnDelay)); 
-            portalSpawned = true;
-        }
-
-        /*
-        if (playerCount == 2)
-        {
-            if (setTime > 0)
-            {
-                setTime -= Time.deltaTime;
-            }
-            else if (setTime <= 0)
-            {
-                Time.timeScale = 0.0f;
-            }
-            int minutes = Mathf.FloorToInt(setTime / 60F);
-            int seconds = Mathf.FloorToInt(setTime - minutes * 60);
-            countdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        }
-        */
-      
-
         if (mode == 0)
         {
-            if (PhotonNetwork.IsMasterClient && !portalSpawned && playerCount == 1)
+            if (PhotonNetwork.IsMasterClient && !portalSpawned && playerCount == 2)
             {
-                
                 mode = 1;
-                StartCoroutine("TimerCoroutine");
+                PV.RPC("StartTimerCoroutine", RpcTarget.All);
+                //StartCoroutine("TimerCoroutine");
                 Debug.Log("timertest");
-                
             }
         }
     }
@@ -68,17 +43,15 @@ public class Countdown : MonoBehaviour
     void OnTriggerEnter(Collider coll)
     {
         Debug.Log("충돌");
-        if (coll.gameObject.tag == "Portal"&&mode==1)
-            {
+        if (coll.gameObject.tag == "Portal" && mode == 1)
+        {
             setTime = 20;
             mode = 2;
             countdownText.color = Color.red;
             StartCoroutine("TimerCoroutine");
             Debug.Log("Open Portal");
-           
         }
     }
-
 
     IEnumerator TimerCoroutine()
     {
@@ -97,28 +70,21 @@ public class Countdown : MonoBehaviour
         Debug.Log("timer finish");
         mode = 1;
         yield break;
-
-        //종료시 동작
-
-
     }
 
     [PunRPC]
     private void ShowTimer(int setTime)
     {
-        // 모든 클라이언트에서 호출되어 타이머를 동기화
         Debug.Log("timertest12");
         int minutes = Mathf.FloorToInt(setTime / 60);
         int seconds = Mathf.FloorToInt(setTime - minutes * 60);
         countdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        //countdownText.text = timerValue.ToString();
     }
 
-
-    IEnumerator DelayedSpawn(float delay)
+    [PunRPC]
+    void StartTimerCoroutine()
     {
-        yield return new WaitForSeconds(delay); // 지정 시간 대기
-        spawnPortal.SpawnObject(); 
+        StartCoroutine("TimerCoroutine");
     }
-
 }
+
