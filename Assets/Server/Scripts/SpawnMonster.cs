be03cd5f6b49b1bd41344ad1d26eebb82c5674c4
HHW101 <hyewonhwang@gmail.com>
2024-04-08 20:18:03 +0900

    수정

--- Assets/Server/Scripts/SpawnMonster.cs
+++ Assets/Server/Scripts/SpawnMonster.cs
@@ -12,7 +12,7 @@ public class SpawnMonster : MonoBehaviour
     void Start()
     {
         SpawnCrocodile();
-        SpawnCrocodile();
+      //  SpawnCrocodile();
     }
 
     void SpawnCrocodile()
