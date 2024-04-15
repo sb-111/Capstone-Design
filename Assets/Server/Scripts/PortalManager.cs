b25098b86c8222034f1225b7430249a4792e676d
HHW101 <hyewonhwang@gmail.com>
2024-04-15 18:06:34 +0900

    구조물 파괴

---
+++ Assets/Server/Scripts/PortalManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    [SerializeField] int currentHP = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsBreak())
+        {
+            Break();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.LogWarning("충돌");
+        if (other.tag == "Melee")
+        {
+            currentHP -= 10;
+            Debug.LogWarning("무기충돌");
+
+        }
+
+    }
+    private bool IsBreak()
+    {
+        return currentHP <= 0;
+    }
+
+
+    private void Break()
+    {
+        // 파괴
+        Destroy(gameObject);
+    }
+}
