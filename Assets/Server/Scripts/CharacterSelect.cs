fe241b5db904e6982818cbdee0ee55cbb9593d15
HHW101 <hyewonhwang@gmail.com>
2024-05-06 15:52:39 +0900

    Revert "Merge branch 'completeMerge' of https://github.com/sb-111/Capstone-Design into completeMerge"
    This reverts commit 56c60725131483cda5e6c5a29d96a76037fdaf58, reversing
    changes made to 084320d1170d769c7467e33c1e277d74efe14582.

--- Assets/Server/Scripts/CharacterSelect.cs
+++ Assets/Server/Scripts/CharacterSelect.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+
 using Photon.Realtime;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class CharacterSelect : MonoBehaviourPunCallbacks
 {
@@ -14,10 +14,7 @@ public class CharacterSelect : MonoBehaviourPunCallbacks
     public GameObject chara1;
     public GameObject chara2;
 
-    // 추가된 UI 요소
-    public Slider loadingProgressBar;
-    public GameObject loadingUI;
-
+    // Start is called before the first frame update
     void Start()
     {
         character = chara1;
@@ -35,80 +32,44 @@ public class CharacterSelect : MonoBehaviourPunCallbacks
             character = chara1;
         }
     }
-
     void OnChara2(bool _bool)
     {
         if (true)
-        {
+        {        
             character = chara2;
         }
     }
 
-    public void GameStart()
+
+    // Update is called once per frame
+    void Update()
     {
-        loadingUI.SetActive(true);
+        
+    }
+    public void GameStart() 
+    {
         PhotonNetwork.JoinRandomRoom();
     }
-
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log("방생성");
+
+        //방 생성
         PhotonNetwork.CreateRoom(null, new RoomOptions());
     }
 
     public override void OnJoinedRoom()
     {
         Debug.Log("Joined a room.");
-        // 코루틴을 사용하여 씬 로딩 시작
-        StartCoroutine(LoadScene_Coroutine("MainScene"));
+
+        PhotonNetwork.LoadLevel("MainScene");//씬 이름
+    
+        //StartCoroutine(LoadLevelWithProgress("MainScene"));
+
+        Debug.Log("방 들어감");
+        // PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0, 1, 0), Quaternion.identity);
+
     }
 
-    // SimpleLauncher에서 가져온 LoadScene_Coroutine
-    IEnumerator LoadScene_Coroutine(string scene)
-    {
-        loadingProgressBar.value = 0;
-        loadingUI.SetActive(true);
-        float progress = 0;
-        PhotonNetwork.LoadLevel(scene);
-        while (!PhotonNetwork.IsConnected)
-        {
-            progress = Mathf.MoveTowards(progress, PhotonNetwork.LevelLoadingProgress, Time.deltaTime * 0.5f);
-            loadingProgressBar.value = progress;
-            if (progress >= 0.9f)
-            {
-                loadingProgressBar.value = 1;
-            }
-            yield return null;
-        }
-        loadingUI.SetActive(false);
-    }
 
-    // SimpleLauncher에서 가져온 LoadLevelWithProgress
-    IEnumerator LoadLevelWithProgress(string sceneName)
-    {
-        loadingUI.SetActive(true);
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-        asyncLoad.allowSceneActivation = false;
-        loadingProgressBar.value = 0;
-        float targetProgress = 0;
-        float fillSpeed = 0.5f;
-        while (!asyncLoad.isDone)
-        {
-            if (asyncLoad.progress < 0.9f)
-            {
-                targetProgress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
-                loadingProgressBar.value = Mathf.MoveTowards(loadingProgressBar.value, targetProgress, fillSpeed * Time.deltaTime);
-            }
-            else
-            {
-                loadingProgressBar.value = Mathf.MoveTowards(loadingProgressBar.value, 1.0f, fillSpeed * Time.deltaTime);
-                if (loadingProgressBar.value >= 1.0f)
-                {
-                    asyncLoad.allowSceneActivation = true;
-                }
-            }
-            yield return null;
-        }
-        loadingUI.SetActive(false);
-    }
 }
