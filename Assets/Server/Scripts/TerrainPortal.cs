35f764a84551ab4c2921742a08b269992bd1ed76
aodlf452 <aodlf452@gmail.com>
2024-05-12 17:16:51 +0900

    minimap import
    플레이어 프리팹 못받음

--- Assets/Server/Scripts/TerrainPortal.cs
+++ Assets/Server/Scripts/TerrainPortal.cs
@@ -1,44 +1,19 @@
 using UnityEngine;
-using Photon.Pun; 
+using Photon.Pun;
 
 public class TerrainPortal : MonoBehaviour
 {
-    public Transform player;
     public Transform receiver;
 
     private bool playerIsOverlapping = false;
-
-    void Start() 
-    {
-        FindLocalPlayer();
-    }
-
-    void FindLocalPlayer()
-    {
-        GameObject localPlayerObj = null;
-        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Player"))
-        {
-            if (obj.GetComponent<PhotonView>().IsMine)
-            {
-                localPlayerObj = obj;
-                break;
-            }
-        }
-        if (localPlayerObj != null)
-        {
-            player = localPlayerObj.transform;
-        }
-        else
-        {
-            Debug.LogWarning("로컬 플레이어를 찾을 수 없습니다.");
-        }
-    }
+    private Transform overlappingPlayer = null; // 겹치는 플레이어를 저장할 변수
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && other.gameObject.GetComponent<PhotonView>().IsMine)
         {
             playerIsOverlapping = true;
+            overlappingPlayer = other.transform; // 겹치는 플레이어의 Transform을 저장
         }
     }
 
@@ -47,14 +22,15 @@ public class TerrainPortal : MonoBehaviour
         if (other.tag == "Player" && other.gameObject.GetComponent<PhotonView>().IsMine)
         {
             playerIsOverlapping = false;
+            overlappingPlayer = null; // 겹치는 플레이어 정보를 초기화
         }
     }
 
     void Update()
     {
-        if (playerIsOverlapping)
+        if (playerIsOverlapping && overlappingPlayer != null)
         {
-            Vector3 portalToPlayer = player.position - transform.position;
+            Vector3 portalToPlayer = overlappingPlayer.position - transform.position;
             float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
 
             // 플레이어가 포탈을 통과했는지 확인
@@ -63,12 +39,13 @@ public class TerrainPortal : MonoBehaviour
                 // 플레이어를 다른 포탈의 위치로 이동
                 float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
                 rotationDiff += 180;
-                player.Rotate(Vector3.up, rotationDiff);
+                overlappingPlayer.Rotate(Vector3.up, rotationDiff);
 
                 Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-                player.position = receiver.position + positionOffset;
+                overlappingPlayer.position = receiver.position + positionOffset;
 
                 playerIsOverlapping = false;
+                overlappingPlayer = null; // 이동 후 겹치는 플레이어 정보 초기화
             }
         }
     }
