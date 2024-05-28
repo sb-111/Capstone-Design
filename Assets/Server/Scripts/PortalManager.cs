7abf199b5476eb2967c9330b016208c59e12dfbb
HHW101 <hyewonhwang@gmail.com>
2024-05-28 13:09:09 +0900

    수정본

--- Assets/Server/Scripts/PortalManager.cs
+++ Assets/Server/Scripts/PortalManager.cs
@@ -8,8 +8,9 @@ public class PortalManager : MonoBehaviourPun
 {
     [SerializeField] int currentHP = 100;
     [SerializeField] GameObject[] enemies;
-    [SerializeField] int fmax = 1;
-    [SerializeField] int ftime = 3;
+    [SerializeField] int[] max;
+    [SerializeField] int[] time;
+    public int gameTime;
     [SerializeField] int smax = 3;
     [SerializeField] int stime = 2;
     [SerializeField] int tmax = 5;
@@ -20,7 +21,7 @@ public class PortalManager : MonoBehaviourPun
     public GameObject fportal;
     public GameObject sportal;
     int mode = 0;
-    int line = 1;
+    int phase = 1;
     int monnum = 0;
     // Start is called before the first frame update
     void Start()
@@ -36,27 +37,66 @@ public class PortalManager : MonoBehaviourPun
         mot.gameObject.SetActive(false);
         fportal.gameObject.SetActive(false);
         sportal.gameObject.SetActive(false);
-        Invoke("monSpawn", waittime+10);
-        SpawnManager.Instance.TimerDestroy();
-        GameManager.Instance.setTime = waittime + 10;
-        SpawnManager.Instance.TimerSpawn();
-        line = 0;
-
-
+        isGame = true;
+        phase = 0;
+        StartCoroutine("Defense");
+     
     }
+    IEnumerator Defense()
+    {
+        while (true)
+        {
+            Debug.Log(phase + "코루틴");
+            if (phase == 2)
+            {
+                GameFinish();
+                yield break;
+            }
+            else if (!isGame)
+            {
+                Debug.Log(phase + "라");
+                SpawnManager.Instance.TimerDestroy();
+                GameManager.Instance.setTime = gameTime;
+                SpawnManager.Instance.TimerSpawn();
+                for (int i = 0; i < enemies.Length; i++)
+                {
+                    enemies[i].gameObject.SetActive(true);
+                    enemies[i].GetComponent<MonsterEndSpawn>().GetStarted(max[phase], time[phase]);
+                }
+                yield return new WaitForSeconds(gameTime);
+                phase++;
+                monDelete();
+            }
+            else if (isGame)
+            {
+                Debug.Log(phase + "휴식");
+                SpawnManager.Instance.TimerDestroy();
+                GameManager.Instance.setTime = waittime;
+                SpawnManager.Instance.TimerSpawn();
+                yield return new WaitForSeconds(waittime);
+            }
+            isGame = !isGame;
+        }
+    }
+
     void monSpawn()
     {
         Debug.Log("1라");
         for (int i = 0; i < enemies.Length; i++)
         {
             enemies[i].gameObject.SetActive(true);
-            enemies[i].GetComponent<MonsterEndSpawn>().GetStarted(fmax, ftime);
+           // enemies[i].GetComponent<MonsterEndSpawn>().GetStarted(fmax, ftime);
         }
-        line = 1;
+        phase = 1;
         isGame = true;
+        Invoke("Wait", waittime);
+        SpawnManager.Instance.TimerDestroy();
+        GameManager.Instance.setTime = waittime;
+        SpawnManager.Instance.TimerSpawn();
     }
     void monSpawn2()
     {
+        monDelete();
         Debug.Log("2라");
         mot.gameObject.SetActive(true);
         fportal.gameObject.SetActive(true);
@@ -66,11 +106,16 @@ public class PortalManager : MonoBehaviourPun
 
             enemies[i].GetComponent<MonsterEndSpawn>().GetStarted(smax, stime);
         }
-        line = 2;
+        phase = 2;
         isGame = true;
+        Invoke("Wait", waittime);
+        SpawnManager.Instance.TimerDestroy();
+        GameManager.Instance.setTime = waittime;
+        SpawnManager.Instance.TimerSpawn();
     }
     void monSpawn3()
     {
+        monDelete();
         sportal.gameObject.SetActive(true);
         Debug.Log("3라");
         for (int i = 0; i < enemies.Length; i++)
@@ -78,12 +123,21 @@ public class PortalManager : MonoBehaviourPun
 
             enemies[i].GetComponent<MonsterEndSpawn>().GetStarted(tmax, ttime);
         }
-        line = 3;
+        phase = 3;
         isGame = true;
+    }
+    void monDelete()
+    {
+        GameObject[] mons = GameObject.FindGameObjectsWithTag("MonsterEnemy");
+        foreach (GameObject mon in mons)
+        {
+
+            PhotonNetwork.Destroy(mon);
+        }
     }
     void GameFinish()
     {
-
+        monDelete();
         GameManager.Instance.GameFinish();
     }
     int MonCheck()
@@ -100,36 +154,42 @@ public class PortalManager : MonoBehaviourPun
     void Update()
     {
      
-        if (isGame&& MonCheck()==0)
-        {
-            isGame = false;
-            switch (line)
-            {
-                case 1:
-                    Invoke("monSpawn2", waittime);
-                    SpawnManager.Instance.TimerDestroy();
-                    GameManager.Instance.setTime = waittime;
-                    SpawnManager.Instance.TimerSpawn();
-                    break;
-                case 2:
-                    Invoke("monSpawn3", waittime);
-                    SpawnManager.Instance.TimerDestroy();
-                    GameManager.Instance.setTime = waittime;
-                    SpawnManager.Instance.TimerSpawn();
-                    break;
-                case 3:
-                    GameFinish();
-                    break;
-            }
+        //if (isGame&& MonCheck()==0)
+        //{
+        //    isGame = false;
+        //    switch (line)
+        //    {
+        //        case 1:
+        //            Invoke("monSpawn2", waittime);
+        //            SpawnManager.Instance.TimerDestroy();
+        //            GameManager.Instance.setTime = waittime;
+        //            SpawnManager.Instance.TimerSpawn();
+        //            break;
+        //        case 2:
+        //            Invoke("monSpawn3", waittime);
+        //            SpawnManager.Instance.TimerDestroy();
+        //            GameManager.Instance.setTime = waittime;
+        //            SpawnManager.Instance.TimerSpawn();
+        //            break;
+        //        case 3:
+        //            GameFinish();
+        //            break;
+        //    }
 
 
-        }
+        //}
         if (IsBreak())
         {
             Break();
         }
   
     }
+
+    void OnDestroy()
+    {
+        SpawnManager.Instance.PortalSpawnerSpawn();
+    }
+
 
     void OnTriggerEnter(Collider coll)
     {
