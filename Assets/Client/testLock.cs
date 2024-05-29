202b88d546768c64ea5df0cb74fadb137602aec5
sb-111 <117798778+sb-111@users.noreply.github.com>
2024-05-29 21:16:01 +0900

    몬스터 기능 영상촬영용 씬 및 스크립트

---
+++ Assets/Client/testLock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testLock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Screen.fullScreen = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
