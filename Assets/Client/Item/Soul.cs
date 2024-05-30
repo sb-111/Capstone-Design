6ec48d816ca6f2b0ac2fe0c836b3ca5a92193985
HHW101 <hyewonhwang@gmail.com>
2024-05-30 17:51:49 +0900

    수정

--- Assets/Client/Item/Soul.cs
+++ Assets/Client/Item/Soul.cs
@@ -35,8 +35,8 @@ public class Soul : MonoBehaviourPun
             growthSystem.GetSoul(soulAmount); // ÇÃ·¹ÀÌ¾î È¹µæ Ã³¸®
 
             PlaySound();
-            GameManager.Instance.ObjDelete(gameObject);
-            //PhotonNetwork.Destroy(gameObject);
+            Destroy(gameObject);
+            //PhotonNetwork.
         }
     }
     /// <summary>
